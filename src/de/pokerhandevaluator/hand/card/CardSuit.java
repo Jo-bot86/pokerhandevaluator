0c9c9dc144c1bb609c7b59b6aed8a4713c7de577
Josef Weldemariam <j.weldemariam@web.de>
2021-12-21 07:13:06 +0100

    add doc to CardSuit and CardValue

--- src/de/pokerhandevaluator/hand/card/CardSuit.java
+++ src/de/pokerhandevaluator/hand/card/CardSuit.java
@@ -1,5 +1,10 @@
 package de.pokerhandevaluator.hand.card;
 
+/**
+ * Represents the four different suits of a Card object
+ * @author Josef Weldemariam
+ *
+ */
 public enum CardSuit {
 	CLUBS, DIAMONDS, HEARTS, SPADES
 }
--- src/de/pokerhandevaluator/hand/card/CardValue.java
+++ src/de/pokerhandevaluator/hand/card/CardValue.java
@@ -1,5 +1,10 @@
 package de.pokerhandevaluator.hand.card;
 
+/**
+ * Represents the 13 different values of a Card object
+ * @author Josef Weldemariam
+ *
+ */
 public enum CardValue {
 	TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE
 }
