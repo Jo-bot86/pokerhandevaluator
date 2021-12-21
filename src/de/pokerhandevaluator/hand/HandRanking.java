05a74cd962ac005f1e88f6f9b679aef07acfd1dc
Josef Weldemariam <j.weldemariam@web.de>
2021-12-21 20:04:49 +0100

    add HandRanking class for rating poker hands

---
+++ src/de/pokerhandevaluator/hand/HandRanking.java
@@ -0,0 +1,12 @@
+package de.pokerhandevaluator.hand;
+
+/**
+ * Represents the ranking of a poker hand
+ * 
+ * @author Josef Weldemariam
+ *
+ */
+public enum HandRanking {
+	HIGH_CARD, ONE_PAIR, TWO_PAIR, THREE_OF_A_KIND, STRAIGHT, FLUSH, FULL_HOUSE,
+	FOUR_OF_A_KIND, STRAIGHT_FLUSH, ROYAL_FLUSH
+}
