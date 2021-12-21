4e32d0a8c2ae286860ce2be3550b72875d59d57c
Josef Weldemariam <j.weldemariam@web.de>
2021-12-21 07:15:50 +0100

    add methods containsFlush, containsStreet and corresponding tests

--- src/de/pokerhandevaluator/hand/Hand.java
+++ src/de/pokerhandevaluator/hand/Hand.java
@@ -13,20 +13,25 @@ import static de.pokerhandevaluator.hand.card.CardValue.SIX;
 import static de.pokerhandevaluator.hand.card.CardValue.TEN;
 import static de.pokerhandevaluator.hand.card.CardValue.THREE;
 import static de.pokerhandevaluator.hand.card.CardValue.TWO;
+import static de.pokerhandevaluator.hand.card.CardSuit.*;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import de.pokerhandevaluator.hand.card.Card;
 import de.pokerhandevaluator.hand.card.CardValue;
 
 /**
- * Represents a hand in a poker game. 
+ * Represents a hand in a poker game.
+ * 
  * @author Josef Weldemariam
  *
  */
 public class Hand {
-	private List<Card> currentHand;
-	
+	private final List<Card> currentHand;
+
 	public Hand(Card card1, Card card2, Card card3, Card card4, Card card5) {
 		currentHand = List.of(card1, card2, card3, card4, card5);
 	}
@@ -35,26 +40,55 @@ public class Hand {
 		return currentHand;
 	}
 
-	public void setCurrentHand(List<Card> currentHand) {
-		this.currentHand = currentHand;
-	}
-	
 	/**
-	 * returns the card with the highest CardValue
-	 * @return
+	 * Determines the card with the highest CardValue
+	 * 
+	 * @return the card from currentHand with the highest CardValue
 	 */
 	public Card getHighestCard() {
 		Card highestCard = getCurrentHand().get(0);
 		Card tmpCard;
-		for(int i =1; i< currentHand.size(); i++) {
+		for (int i = 1; i < currentHand.size(); i++) {
 			tmpCard = currentHand.get(i);
-			if(tmpCard.getCardValue().compareTo(highestCard.getCardValue()) > 0) {
+			if (tmpCard.getCardValue().compareTo(highestCard.getCardValue()) > 0) {
 				highestCard = tmpCard;
 			}
 		}
-		
+
 		return highestCard;
 	}
-	
-	
+
+	/**
+	 * Checks if the currentHand contains a flush returns true if currentHand
+	 * contains a flush otherwise false
+	 */
+	public boolean containsFlush() {
+		Card firstCard = getCurrentHand().get(0);
+		List<Card> potentialFlushHand = currentHand.stream().filter(
+				card -> card.getCardSuit().compareTo(firstCard.getCardSuit()) == 0)
+				.collect(Collectors.toList());
+		if (potentialFlushHand.size() == 5) {
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Checks if the currentHand contains a street
+	 * 
+	 * @return true only if the hand contains a street otherwise false
+	 */
+	public boolean containsStreet() {
+		List<Card> sortedHand = new ArrayList<Card>(currentHand);
+		Collections.sort(sortedHand,
+				(card1, card2) -> card1.getCardValue().compareTo(card2.getCardValue()));
+		for (int i = 0; i < sortedHand.size() - 1; i++) {
+			if (sortedHand.get(i).getCardValue()
+					.ordinal() != sortedHand.get(i + 1).getCardValue().ordinal() - 1) {
+				return false;
+			}
+		}
+		return true;
+	}
+
 }
--- src/test/hand/HandTest.java
+++ src/test/hand/HandTest.java
@@ -11,42 +11,106 @@ import de.pokerhandevaluator.hand.card.Card;
 
 public class HandTest {
 	Hand hand;
-	
+
 	@Test
 	public void getHighestCardTest_Ace_At_First_Position() {
-		hand = new Hand(new Card(CLUBS, ACE), new Card(CLUBS, TWO), new Card(CLUBS, THREE),
-				new Card(CLUBS, TEN), new Card(DIAMONDS, KING));
-		
+		hand = new Hand(new Card(CLUBS, ACE), new Card(CLUBS, TWO),
+				new Card(CLUBS, THREE), new Card(CLUBS, TEN), new Card(DIAMONDS, KING));
+
 		assertEquals(ACE, hand.getHighestCard().getCardValue());
 		assertEquals(CLUBS, hand.getHighestCard().getCardSuit());
 	}
-	
+
 	@Test
 	public void getHighestCardTest_Ace_At_Last_Position() {
-		hand = new Hand(new Card(CLUBS, FIVE), new Card(CLUBS, TWO), new Card(CLUBS, THREE),
-				new Card(CLUBS, TEN), new Card(DIAMONDS, ACE));
-		
+		hand = new Hand(new Card(CLUBS, FIVE), new Card(CLUBS, TWO),
+				new Card(CLUBS, THREE), new Card(CLUBS, TEN), new Card(DIAMONDS, ACE));
+
 		assertEquals(ACE, hand.getHighestCard().getCardValue());
 		assertEquals(DIAMONDS, hand.getHighestCard().getCardSuit());
 	}
-	
+
 	@Test
 	public void getHighestCardTest_Ace_At_Middle_Position() {
-		hand = new Hand(new Card(CLUBS, FIVE), new Card(CLUBS, TWO), new Card(HEARTS, ACE),
-				new Card(CLUBS, TEN), new Card(DIAMONDS, QUEEN));
-		
+		hand = new Hand(new Card(CLUBS, FIVE), new Card(CLUBS, TWO),
+				new Card(HEARTS, ACE), new Card(CLUBS, TEN), new Card(DIAMONDS, QUEEN));
 		assertEquals(ACE, hand.getHighestCard().getCardValue());
 		assertEquals(HEARTS, hand.getHighestCard().getCardSuit());
 	}
-	
+
 	@Test
 	public void getHighestCardTest_Ace_At_FirstANDLast_Position() {
-		hand = new Hand(new Card(CLUBS, ACE), new Card(SPADES, FOUR), new Card(DIAMONDS, SEVEN),
-				new Card(CLUBS, JACK), new Card(DIAMONDS, ACE));
-		
+		hand = new Hand(new Card(CLUBS, ACE), new Card(SPADES, FOUR),
+				new Card(DIAMONDS, SEVEN), new Card(CLUBS, JACK),
+				new Card(DIAMONDS, ACE));
+
 		assertEquals(ACE, hand.getHighestCard().getCardValue());
 		assertEquals(CLUBS, hand.getHighestCard().getCardSuit());
 	}
+
+//	*****************************************************************************************************
+
+	@Test
+	public void containsFlushTest_FlushOfClubs() {
+		hand = new Hand(new Card(CLUBS, TWO), new Card(CLUBS, FOUR),
+				new Card(CLUBS, SEVEN), new Card(CLUBS, JACK), new Card(CLUBS, ACE));
+		assertEquals(true, hand.containsFlush());
+	}
+
+	@Test
+	public void containsFlushTest_FlushOfHearts() {
+		hand = new Hand(new Card(HEARTS, TWO), new Card(HEARTS, FOUR),
+				new Card(HEARTS, SEVEN), new Card(HEARTS, JACK), new Card(HEARTS, ACE));
+		assertEquals(true, hand.containsFlush());
+	}
+
+	@Test
+	public void containsFlushTest_OneCardMissing() {
+		hand = new Hand(new Card(DIAMONDS, TWO), new Card(HEARTS, FOUR),
+				new Card(HEARTS, SEVEN), new Card(HEARTS, JACK), new Card(HEARTS, ACE));
+		assertEquals(false, hand.containsFlush());
+	}
+
+	@Test
+	public void containsFlushTest_TwoCardsOfSameSuits() {
+		hand = new Hand(new Card(DIAMONDS, TWO), new Card(HEARTS, FOUR),
+				new Card(CLUBS, SEVEN), new Card(SPADES, JACK), new Card(HEARTS, ACE));
+		assertEquals(false, hand.containsFlush());
+	}
+
+//	*********************************************************************************************************
+
+	@Test
+	public void containsStreetTest_Street10ToAce_Orderd() {
+		hand = new Hand(new Card(DIAMONDS, TEN), new Card(HEARTS, JACK),
+				new Card(CLUBS, QUEEN), new Card(SPADES, KING), new Card(HEARTS, ACE));
+		assertEquals(true, hand.containsStreet());
+	}
+
+	@Test
+	public void containsStreetTest_Street10ToAce_Unorderd() {
+		hand = new Hand(new Card(SPADES, KING), new Card(HEARTS, JACK),
+				new Card(HEARTS, ACE), new Card(CLUBS, QUEEN), new Card(DIAMONDS, TEN));
+		assertEquals(true, hand.containsStreet());
+	}
+
+	@Test
+	public void containsStreetTest_Street10ToAce_OrderReversed() {
+		hand = new Hand(new Card(HEARTS, ACE), new Card(SPADES, KING),
+				new Card(CLUBS, QUEEN), new Card(HEARTS, JACK), new Card(DIAMONDS, TEN));
+		assertEquals(true, hand.containsStreet());
+	}
 	
-	
+	public void containsStreetTest_Street2To6_Ordered() {
+		hand = new Hand(new Card(DIAMONDS, TWO), new Card(HEARTS, THREE),
+				new Card(CLUBS, FOUR), new Card(SPADES, FIVE), new Card(HEARTS, SIX));
+		assertEquals(true, hand.containsStreet());
+	}
+
+	@Test
+	public void containsStreetTest_NoStreet_OneMissing() {
+		hand = new Hand(new Card(HEARTS, TWO), new Card(CLUBS, QUEEN),
+				new Card(HEARTS, JACK), new Card(SPADES, KING), new Card(DIAMONDS, TEN));
+		assertEquals(false, hand.containsStreet());
+	}
 }
