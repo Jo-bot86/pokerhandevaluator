2041d9c8807cf5993774e8e550114b159682afae
Josef Weldemariam <j.weldemariam@web.de>
2021-12-23 21:47:44 +0100

    remove unnecessary tests and add two test cases for compareHighCardsList

--- src/test/CalculatorTest.java
+++ src/test/CalculatorTest.java
@@ -4,87 +4,79 @@ import static de.pokerhandevaluator.hand.card.CardSuit.*;
 import static de.pokerhandevaluator.hand.card.CardValue.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 import de.pokerhandevaluator.Calculator;
 import de.pokerhandevaluator.hand.Hand;
 import de.pokerhandevaluator.hand.card.Card;
+import de.pokerhandevaluator.hand.card.CardValue;
 
 public class CalculatorTest {
 	Hand hand1;
 	Hand hand2;
 	Calculator calculator;
-	
-//	@Test
-//	public void calculateTest_hand1Flush_hand2RoyalFlush() {
-//		hand1 = new Hand(new Card(HEARTS, KING), new Card(HEARTS, SEVEN),
-//				new Card(HEARTS, FIVE), new Card(HEARTS, TWO), new Card(HEARTS, ACE));
-//		hand2 = new Hand(new Card(SPADES, JACK), new Card(SPADES, ACE),
-//				new Card(SPADES, QUEEN), new Card(SPADES, TEN), new Card(SPADES, KING));
-//		calculator = new Calculator(hand1,hand2);
-//		calculator.calculate();
-//	}
-//	
-//	@Test
-//	public void calculateTest_hand1TwoPairs_hand2HighCard() {
-//		hand1 = new Hand(new Card(DIAMONDS, ACE), new Card(HEARTS, TWO),
-//				new Card(HEARTS, KING), new Card(DIAMONDS, KING), new Card(SPADES, ACE));
-//		hand2 = new Hand(new Card(SPADES, FOUR), new Card(SPADES, TWO),
-//				new Card(DIAMONDS, THREE), new Card(DIAMONDS, TEN), new Card(SPADES, NINE));
-//		calculator = new Calculator(hand1,hand2);
-//		calculator.calculate();
-//	}
-	
+
+	@Test
+	public void calculateTest_hand1Flush_hand2RoyalFlush() {
+		hand1 = new Hand(new Card(HEARTS, KING), new Card(HEARTS, SEVEN),
+				new Card(HEARTS, FIVE), new Card(HEARTS, TWO), new Card(HEARTS, ACE));
+		hand2 = new Hand(new Card(SPADES, JACK), new Card(SPADES, ACE),
+				new Card(SPADES, QUEEN), new Card(SPADES, TEN), new Card(SPADES, KING));
+		calculator = new Calculator(hand1, hand2);
+		System.out.println(calculator.calculate());
+	}
+
+	@Test
+	public void calculateTest_hand1TwoPairs_hand2HighCard() {
+		hand1 = new Hand(new Card(DIAMONDS, ACE), new Card(HEARTS, TWO),
+				new Card(HEARTS, KING), new Card(DIAMONDS, KING), new Card(SPADES, ACE));
+		hand2 = new Hand(new Card(SPADES, FOUR), new Card(SPADES, TWO),
+				new Card(DIAMONDS, THREE), new Card(DIAMONDS, TEN),
+				new Card(SPADES, NINE));
+		calculator = new Calculator(hand1, hand2);
+		System.out.println(calculator.calculate());
+	}
+
 // *********************************************************************************************
-	
+
 	@Test
-	public void getHighCardTest_FourOfAKindAndAce() {
-		hand1 = new Hand(new Card(HEARTS, FOUR), new Card(SPADES, FOUR),
-				new Card(DIAMONDS, FOUR), new Card(HEARTS, ACE), new Card(CLUBS, FOUR));
-		calculator = new Calculator();
-		assertEquals(ACE, calculator.getHighCard(hand1));
+	public void compareHighCardsListTest_hand1HighCardTen_hand2_HighCardJack() {
+		hand1 = new Hand(new Card(HEARTS, TWO), new Card(SPADES, FOUR),
+				new Card(CLUBS, TEN), new Card(HEARTS, FIVE), new Card(HEARTS, SIX));
+		hand2 = new Hand(new Card(SPADES, JACK), new Card(SPADES, TWO),
+				new Card(SPADES, THREE), new Card(DIAMONDS, EIGHT),
+				new Card(SPADES, NINE));
+		calculator = new Calculator(hand1, hand2);
+		
+		List<CardValue> cardValueshand1 = calculator.mapToCardValue(hand1.getCurrentHand());
+		List<CardValue> cardValueshand2 = calculator.mapToCardValue(hand2.getCurrentHand());
+		
+		List<CardValue> withoutDuplicates1 = calculator.removeDuplicates(cardValueshand1);
+		List<CardValue> withoutDuplicates2 = calculator.removeDuplicates(cardValueshand2);
+
+		assertEquals("Hand 2 wins with HIGH_CARD", calculator.compareHighCardsList(withoutDuplicates1, withoutDuplicates2));
+		
 	}
 	
 	@Test
-	public void getHighCardTest_FourOfAKindAndTwo() {
-		hand1 = new Hand(new Card(HEARTS, FOUR), new Card(SPADES, FOUR),
-				new Card(DIAMONDS, FOUR), new Card(HEARTS, TWO), new Card(CLUBS, FOUR));
-		calculator = new Calculator();
-		assertEquals(TWO, calculator.getHighCard(hand1));
+	public void compareHighCardsListTest_hand1HighCardKing_hand2_HighCardQueen() {
+		hand1 = new Hand(new Card(HEARTS, JACK), new Card(SPADES, FOUR),
+				new Card(CLUBS, TEN), new Card(HEARTS, KING), new Card(HEARTS, SIX));
+		hand2 = new Hand(new Card(SPADES, JACK), new Card(SPADES, QUEEN),
+				new Card(SPADES, THREE), new Card(DIAMONDS, EIGHT),
+				new Card(SPADES, NINE));
+		calculator = new Calculator(hand1, hand2);
+		
+		List<CardValue> cardValueshand1 = calculator.mapToCardValue(hand1.getCurrentHand());
+		List<CardValue> cardValueshand2 = calculator.mapToCardValue(hand2.getCurrentHand());
+		
+		List<CardValue> withoutDuplicates1 = calculator.removeDuplicates(cardValueshand1);
+		List<CardValue> withoutDuplicates2 = calculator.removeDuplicates(cardValueshand2);
+
+		assertEquals("Hand 1 wins with HIGH_CARD", calculator.compareHighCardsList(withoutDuplicates1, withoutDuplicates2));
+		
 	}
-	
-	@Test
-	public void getHighCardTest_TwoPairsAndAce() {
-		hand1 = new Hand(new Card(HEARTS, FOUR), new Card(SPADES, FOUR),
-				new Card(DIAMONDS, SIX), new Card(HEARTS, ACE), new Card(CLUBS, SIX));
-		calculator = new Calculator();
-		assertEquals(ACE, calculator.getHighCard(hand1));
-	}
-	
-	@Test
-	public void getHighCardTest_TwoPairsAndTwo() {
-		hand1 = new Hand(new Card(HEARTS, FOUR), new Card(SPADES, FOUR),
-				new Card(DIAMONDS, SIX), new Card(HEARTS, TWO), new Card(CLUBS, SIX));
-		calculator = new Calculator();
-		assertEquals(TWO, calculator.getHighCard(hand1));
-	}
-	
-	@Test
-	public void getHighCardTest_ThreeOfAKindAndQueenAndJack() {
-		hand1 = new Hand(new Card(HEARTS, FOUR), new Card(SPADES, FOUR),
-				new Card(DIAMONDS, FOUR), new Card(HEARTS, QUEEN), new Card(CLUBS, JACK));
-		calculator = new Calculator();
-		assertEquals(QUEEN, calculator.getHighCard(hand1));
-	}
-	
-	@Test
-	public void getHighCardTest_ThreeOfAKindAndTwoAndEight() {
-		hand1 = new Hand(new Card(HEARTS, KING), new Card(SPADES, KING),
-				new Card(DIAMONDS, TWO), new Card(HEARTS, EIGHT), new Card(CLUBS, KING));
-		calculator = new Calculator();
-		assertEquals(EIGHT, calculator.getHighCard(hand1));
-	}
-	
-	
-	
+
 }
